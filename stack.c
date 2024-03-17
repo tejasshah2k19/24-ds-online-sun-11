e62551d9887d9e83c7b58f7a52ecaa72aa9b804d
Tejas Shah <tejasshah2k19@gmail.com>
2024-03-17 11:49:13 +0530

    push display

---
+++ stack.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include<stdlib.h> 
+#define SIZE 5
+int stack[SIZE];
+
+int top = -1; // empty
+ 
+void push(int num)
+{
+    if (top == SIZE - 1)
+    {
+        printf("\nStack OVERFLOW");
+    }
+    else
+    {
+        top++;
+        stack[top] = num;
+    }
+}
+
+void display(){
+    int i; 
+    if(top == -1 ){
+        printf("\nSTack UnderFlow");
+    }else{
+        printf("\nElement(s) in the STACK\n");
+        for(i=top; i>=0 ;i--){
+            printf("\n%d",stack[i]);
+        }
+    }
+}
+
+int main(){
+
+    int choice;
+    int num; 
+
+    while(-1){ //0:false anynumber:true 
+        printf("\n0 For EXIT\n1 For PUSH\n2 For Display\nEnter your hoice");
+        scanf("%d",&choice); 
+
+        switch (choice)
+        {
+        case 1:
+            printf("\nEnter number ");
+            scanf("%d",&num); 
+            push(num); 
+            break;
+        case 2:
+            display();
+            break;
+        
+        default:
+            printf("\nInvalid choice");
+            break;
+        case 0:
+            exit(0);
+        }
+    }
+
+
+    return 0; 
+}
+
+
+
