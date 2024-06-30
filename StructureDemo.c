d35ed80aeb264c358cb9aeba896cdf4c2e7d2f00
Tejas Shah <tejasshah2k19@gmail.com>
2024-06-30 10:54:36 +0530

    array of structure sort

--- StructureDemo.c
+++ StructureDemo.c
@@ -14,8 +14,8 @@ struct employee
 int main()
 {
 
-    struct employee s[10];
-    int i,maxIndex;
+    struct employee s[10], tmp;
+    int i, maxIndex, j;
     int maxSalary;
 
     for (i = 0; i < 3; i++)
@@ -37,17 +37,38 @@ int main()
     // highest salary paid : max salary
     // rock : 55000
     maxSalary = s[0].salary;
-    maxIndex = 0; 
+    maxIndex = 0;
     for (i = 0; i < 3; i++)
     {
-        if(maxSalary< s[i].salary){
+        if (maxSalary < s[i].salary)
+        {
             maxSalary = s[i].salary;
-            maxIndex = i; 
+            maxIndex = i;
         }
     }
 
-    printf("\nMaxSalary = %d",maxSalary);
-    printf("\nEmp Name = %s",s[maxIndex].empName);
- 
+    printf("\nMaxSalary = %d", maxSalary);
+    printf("\nEmp Name = %s", s[maxIndex].empName);
+
+    // sorting -> salary asc {array of structure}
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
+            if (s[j].salary > s[j + 1].salary)
+            {
+                tmp = s[j];
+                s[j] = s[j + 1];
+                s[j + 1] = tmp;
+            }
+        }
+    }
+    printf("\nAFter Sorting:");
+    printf("\nName         DeptName        BasicSalary   Salary");
+    for (i = 0; i < 3; i++)
+    {
+        printf("\n %-20s%-20s%-6d%-6d", s[i].empName, s[i].department, s[i].basicSalary, s[i].salary);
+    }
     return 0;
 }
