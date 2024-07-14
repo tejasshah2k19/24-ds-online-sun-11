6b09d26980373f248468effd87da73d03acc5626
Tejas Shah <tejasshah2k19@gmail.com>
2024-07-14 10:55:28 +0530

    sort array struct pointer

--- DMAStructArray.c
+++ DMAStructArray.c
@@ -15,6 +15,8 @@ int main()
     int totalStudents;
     struct student *s; // array
     int i;
+    int j;
+    struct student tmp;
 
     printf("\nHow many students you want to add!! ");
     scanf("%d", &totalStudents);
@@ -39,10 +41,26 @@ int main()
     // output : John : 1 , rock : 2 , dual : 3, sam : 4
 
     // some code goes here -> that sorts the students in asc of rank
+
+    for (i = 0; i < totalStudents; i++)
+    {
+        for (j = 0; j < totalStudents - 1; j++)
+        {
+            if (s[j].rank > s[j + 1].rank)
+            {
+                tmp = s[j];
+                s[j] = s[j + 1];
+                s[j + 1] = tmp;
+            }
+        }
+    }
+
     printf("\nStudents Details:\n");
     for (i = 0; i < totalStudents; i++)
     {
+        printf("\n**************\n");
         printf("Name = %s\nRollNum = %d\nCity = %s\nRank = %d", s[i].name, s[i].rollNum, s[i].city, s[i].rank);
+        printf("\n**************\n");
     }
 
     free(s);
