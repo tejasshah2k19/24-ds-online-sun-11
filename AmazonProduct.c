6a0a1409fbe46119c9df9184bad812a79b144cee
Tejas Shah <tejasshah2k19@gmail.com>
2024-07-21 10:17:26 +0530

    print

--- AmazonProduct.c
+++ AmazonProduct.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 
 #define SIZE 10
 
@@ -16,9 +16,9 @@ struct product
 
 int main()
 {
-    int choice,i;
-    struct product p[SIZE];//p[0] p[1] 
-    int totalProducts; 
+    int choice, i;
+    struct product p[SIZE]; // p[0] p[1]
+    int totalProducts;
     while (1)
     {
         printf("\n0 For Exit\n1 For Add\n2 For List Products\n3 For Search Product By Category\n4 For Search Product By Id");
@@ -31,26 +31,37 @@ int main()
             exit(0);
         case 1:
             printf("\nHow many Products You want to Enter ");
-            scanf("%d",&totalProducts);//2 {10+}
+            scanf("%d", &totalProducts); // 2 {10+}
 
-            if(totalProducts > 10){
+            if (totalProducts > 10)
+            {
                 printf("\nYou are Allowed to add 10 Products MAX");
-            }else{
-                for(i=0;i<totalProducts;i++){
+            }
+            else
+            {
+                for (i = 0; i < totalProducts; i++)
+                {
                     printf("\nEnter ProductId");
-                    scanf("%d",&p[i].productId);
+                    scanf("%d", &p[i].productId);
                     printf("\nEnter Name and Category");
-                    scanf("%s%s",&p[i].name,&p[i].category);
+                    scanf("%s%s", &p[i].name, &p[i].category);
                     printf("\nEnter price and qty");
-                    scanf("%d%d",&p[i].price,&p[i].qty);
+                    scanf("%d%d", &p[i].price, &p[i].qty);
                     printf("\nEnter discountPercentage and Rating");
-                    scanf("%d%d",&p[i].discountPercentage,&p[i].rating);
-
+                    scanf("%d%d", &p[i].discountPercentage, &p[i].rating);
                 }
             }
             break;
         case 2:
+
+            printf("\nProductId  Name  Category  Price Qty  DiscountPerc  Rating");
+            for (i = 0; i < totalProducts; i++)
+            {
+                printf("\n %d %s %s %d %d %d %d", p[i].productId,p[i].name, p[i].category,p[i].price, p[i].qty,p[i].discountPercentage, p[i].rating);
+            }
             // print all
+            //format 
+            
 
         default:
             break;
