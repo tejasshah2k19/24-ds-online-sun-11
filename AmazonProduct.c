674c598b22ba2e549501725850975d461d7db658
Tejas Shah <tejasshah2k19@gmail.com>
2024-07-21 09:58:40 +0530

    add product static

--- AmazonProduct.c
+++ AmazonProduct.c
@@ -16,8 +16,9 @@ struct product
 
 int main()
 {
-    int choice;
-
+    int choice,i;
+    struct product p[SIZE];//p[0] p[1] 
+    int totalProducts; 
     while (1)
     {
         printf("\n0 For Exit\n1 For Add\n2 For List Products\n3 For Search Product By Category\n4 For Search Product By Id");
@@ -29,7 +30,24 @@ int main()
         case 0:
             exit(0);
         case 1:
-            // take input
+            printf("\nHow many Products You want to Enter ");
+            scanf("%d",&totalProducts);//2 {10+}
+
+            if(totalProducts > 10){
+                printf("\nYou are Allowed to add 10 Products MAX");
+            }else{
+                for(i=0;i<totalProducts;i++){
+                    printf("\nEnter ProductId");
+                    scanf("%d",&p[i].productId);
+                    printf("\nEnter Name and Category");
+                    scanf("%s%s",&p[i].name,&p[i].category);
+                    printf("\nEnter price and qty");
+                    scanf("%d%d",&p[i].price,&p[i].qty);
+                    printf("\nEnter discountPercentage and Rating");
+                    scanf("%d%d",&p[i].discountPercentage,&p[i].rating);
+
+                }
+            }
             break;
         case 2:
             // print all
