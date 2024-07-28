d10e9e076d23e8b6c1b7ec00f8703334da022ebf
Tejas Shah <tejasshah2k19@gmail.com>
2024-07-28 09:37:20 +0530

    amazon struct sort

--- AmazonProduct.c
+++ AmazonProduct.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define SIZE 10
 
@@ -16,18 +17,22 @@ struct product
 
 int main()
 {
-    int choice, i;
+    int choice, i, id, j;
+    int rating;
+    char cat[30];
+    struct product tmp;
     // struct product p[SIZE]; // p[0] p[1]
-    struct product p[]={
-        {1,"iphone 15","phone",20,150000,10,5},
-        {2,"legion y 520","laptop",50,55000,15,4},
-        {3,"vivo v1","phone",100,20000,7,3},
-        
+    struct product p[] = {
+        {1, "iphone 15", "phone", 20, 150000, 10, 5},
+        {2, "legion y 520", "laptop", 50, 55000, 15, 4},
+        {3, "vivo v1", "phone", 100, 20000, 7, 3},
+
     };
-    int totalProducts=3;
+    int totalProducts = 3;
     while (1)
     {
         printf("\n0 For Exit\n1 For Add\n2 For List Products\n3 For Search Product By Category\n4 For Search Product By Id");
+        printf("\n5 For search By Rating");
         printf("\nEnter choice");
         scanf("%d", &choice);
 
@@ -63,12 +68,61 @@ int main()
             printf("\nProductId  Name         Category        Price     Qty    DiscountPerc  Rating");
             for (i = 0; i < totalProducts; i++)
             {
-                printf("\n %-9d %-13s %-13s  %-6d    %-5d     %-13d %-5d", p[i].productId,p[i].name, p[i].category,p[i].price, p[i].qty,p[i].discountPercentage, p[i].rating);
+                printf("\n %-9d %-13s %-13s  %-6d    %-5d     %-13d %-5d", p[i].productId, p[i].name, p[i].category, p[i].price, p[i].qty, p[i].discountPercentage, p[i].rating);
             }
+            break;
             // print all
-            //format 
+            // format
+        case 3:
+            printf("\nEnter Category?");
+            scanf("%s", &cat);
+            // linear search
+            printf("\nProductId  Name         Category        Price     Qty    DiscountPerc  Rating");
+            for (i = 0; i < totalProducts; i++)
+            {
+                // strcmp
+                // phone Phone =>   same { case ignore }
 
+                if (stricmp(p[i].category, cat) == 0)
+                {
+                    printf("\n %-9d %-13s %-13s  %-6d    %-5d     %-13d %-5d", p[i].productId, p[i].name, p[i].category, p[i].price, p[i].qty, p[i].discountPercentage, p[i].rating);
+                }
+            }
+            break;
+        case 4:
+            // linear search
+            printf("\nEnter ProductID ? ");
+            scanf("%d", &id);
+            printf("\nProductId  Name         Category        Price     Qty    DiscountPerc  Rating");
+            for (i = 0; i < totalProducts; i++)
+            {
+                if (p[i].productId == id)
+                {
+                    printf("\n %-9d %-13s %-13s  %-6d    %-5d     %-13d %-5d", p[i].productId, p[i].name, p[i].category, p[i].price, p[i].qty, p[i].discountPercentage, p[i].rating);
+                }
+            }
 
+        case 5:
+            // sort products by rating
+            for (i = 0; i < totalProducts; i++)
+            {
+                for (j = 0; j < totalProducts - 1; j++)
+                {
+                    if (p[j].rating > p[j + 1].rating)
+                    {
+                        tmp = p[j];
+                        p[j] = p[j + 1];
+                        p[j + 1] = tmp;
+                    }
+                }
+            }
+
+            // binary search
+            printf("\nEnter Rating? ");
+            scanf("%d", &rating);
+
+            // logic
+            break;
         default:
             break;
         }
