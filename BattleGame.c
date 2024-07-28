facee56466eda0a23cfdc73d50a3885982d53bdd
Tejas Shah <tejasshah2k19@gmail.com>
2024-07-28 10:33:02 +0530

    BattleGame 1

---
+++ BattleGame.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#define MASTER 8
+
+int master[] = {12, 34, 56, 78, 97, 97, 43, 200};
+
+int playerA[MASTER];
+int playerB[MASTER];
+
+void distribute()
+{
+    // playerA[] = {12,56,97,43}
+
+    // playerB[] = {34,78,97,200}
+}
+
+void printCards()
+{
+    // playerA
+
+    // playerB
+    //  playerA[] = {12,56,97,43}
+
+    // playerB[] = {34,78,97,200}
+}
+
+int main()
+{
+
+    return 0;
+}
