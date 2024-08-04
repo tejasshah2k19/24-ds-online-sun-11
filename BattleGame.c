9854ddd605a2b864b7de6b24876f7fc2936cb393
Tejas Shah <tejasshah2k19@gmail.com>
2024-08-04 09:25:58 +0530

    distribute and print

--- BattleGame.c
+++ BattleGame.c
@@ -11,6 +11,19 @@ void distribute()
     // playerA[] = {12,56,97,43}
 
     // playerB[] = {34,78,97,200}
+
+    int i;
+    int x = 0;
+    int y = 0;
+    for (i = 0; i < MASTER; i++)
+    {
+
+        playerA[x] = master[i];
+        x++;
+        i++;
+        playerB[y] = master[i];
+        y++;
+    }
 }
 
 void printCards()
@@ -19,12 +32,28 @@ void printCards()
 
     // playerB
     //  playerA[] = {12,56,97,43}
-
+    int i;
+    printf("\nPlayerA : ");
+    for (i = 0; i < MASTER; i++)
+    {
+        if(playerA[i] == 0 )
+            break;
+        printf(" %d", playerA[i]);
+    }
     // playerB[] = {34,78,97,200}
+    printf("\nPlayerB : ");
+    for (i = 0; i < MASTER; i++)
+    {
+        
+        if(playerB[i] == 0 )
+            break;
+        printf(" %d", playerB[i]);
+    }
 }
 
 int main()
 {
-
+    distribute();
+    printCards();
     return 0;
 }
